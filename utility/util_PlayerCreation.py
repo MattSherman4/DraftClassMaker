46637f52d8b21f68a07a5b189c78da49cc8ea3cd
MattSherman4 <mattsherman2001@gmail.com>
2026-07-28 14:11:22 -0400

    made cfg for college odds by position

--- utility/util_PlayerCreation.py
+++ utility/util_PlayerCreation.py
@@ -178,57 +178,8 @@ def get_college(size, position, prestige):
                 if college_list_draftable.count(college) < max_pos_draftable[position]:
                     break
         return college
-        
-    beast = load_beast(defunct_colleges = False)
-    beast = beast[beast['POS'] == position]
-    history_draft = load_draft_history()
-    history_draft = history_draft[history_draft['Year'] >= PASSING_ERA]
-    all_colleges = load_college()
 
-    # Get positional draft frequencies
-    history_draft_pos = history_draft[history_draft['POS'] == position]
-    pos_freq = add_missing_divisions((history_draft_pos['College'].value_counts(normalize = True)).to_dict())
-    
-    # Get top rounds frequencies. Use Conf instead of Div to narrow the field more in early rounds
-    history_draft_data_top = history_draft_pos[history_draft_pos['Rnd'] <= draft_grades_round_cutoff['Top']]
-    top_freq = history_draft_data_top['Conference'].value_counts(normalize = True).to_dict()
-    if len(top_freq):
-        top_freq = weights_to_one_dict(top_freq, max(top_freq, key = top_freq.get))
-    else:
-        top_freq = POWER_CONF
-
-    # Get Middle rounds frequencies
-    history_draft_data_mid = history_draft_pos[history_draft_pos['Rnd'] > draft_grades_round_cutoff['Top']]
-    history_draft_data_mid = history_draft_data_mid[history_draft_data_mid['Rnd'] <= draft_grades_round_cutoff['Middle']]
-    if len(history_draft_data_mid):
-        mid_freq = add_missing_divisions((history_draft_data_mid['Division'].value_counts(normalize = True)).to_dict())
-    else:
-        mid_freq = POWER_DIV
-
-    # Get Bottom rounds frequencies
-    history_draft_data_bot = history_draft_pos[history_draft_pos['Rnd'] > draft_grades_round_cutoff['Middle']]
-    if len(history_draft_data_bot):
-        bot_freq = add_missing_divisions((history_draft_data_bot['Division'].value_counts(normalize = True)).to_dict())
-    else:
-        bot_freq = POWER_DIV
-
-    # Get FA rounds frequencies
-    #TODO: Get actual UDFA Data?
-    history_draft_data_FA = beast[beast['Grade'] == 'FA']
-    if len(history_draft_data_FA):
-        FA_freq = add_missing_divisions((history_draft_data_FA['Division'].value_counts(normalize = True)).to_dict())
-    else:
-        FA_freq = POWER_DIV
-
-    # Get RMC rounds frequencies
-    history_draft_data_RMC = beast[beast['Grade'] == 'RMC']
-    if len(history_draft_data_RMC):
-        RMC_freq = add_missing_divisions((history_draft_data_RMC['Division'].value_counts(normalize = True)).to_dict())
-    else:
-        RMC_freq = POWER_DIV
-
-    # Spin top 10 + 'Random'
-    top_10 = weights_to_one_dict(dict(heapq.nlargest(10, pos_freq.items(), key = itemgetter(1))))
+    #! Add stuff here !#
     
     college_list_return = []
     college_list_total = []
